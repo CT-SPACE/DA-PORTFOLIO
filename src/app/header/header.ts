6b367c42c1eba492d3b240b1106e3c5e4804c8c1
C. Troitzsch <150924499+CT-SPACE@users.noreply.github.com>
2025-12-06 15:35:33 +0100

    Header ready

--- src/app/header/header.ts
+++ src/app/header/header.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+ import { Component, signal } from '@angular/core'
 
 @Component({
   selector: 'app-header',
@@ -7,5 +7,48 @@ import { Component } from '@angular/core';
   styleUrl: './header.scss',
 })
 export class Header {
+  frames: string[] = ['assets/img/burger_1.svg', 'assets/img/burger_2.svg', 'assets/img/burger_3.svg', 'assets/img/burger_4.svg', 'assets/img/burger_5.svg'];
+isOpen = signal(false);
+isAnimating = signal(false);
+currentIndex = signal(0);
+currentSrc = signal(this.frames[0]);
+selected: string | null = null;
 
+select(choice: string) {
+  this.selected = choice;
+}
+
+  ngOnInit() {
+    if (typeof window !== 'undefined') {
+      this.preloadFrames();
+    }
+  }
+
+  onToggleMenu(): void {
+    if (this.isAnimating()) return;
+    this.isAnimating.set(true);
+    const forward = !this.isOpen();
+    this.isOpen.set(!this.isOpen());
+    const sequence = forward ? [0,1,2,3,4] : [4,3,2,1,0] ;
+    let i = 0;
+    const stepMs = 120;
+    const timer = setInterval(() => {
+      const idx = sequence[i];
+      this.currentIndex.set(idx);
+      this.currentSrc.set(this.frames[idx]);
+      i++;
+      if (i >= sequence.length) {
+        clearInterval(timer);
+        this.isOpen.set(forward);
+        this.isAnimating.set(false);
+      }
+    }, stepMs);
+  }
+
+  private preloadFrames() {
+    for (const src of this.frames) {
+      const img = new Image();
+      img.src = src;
+    }
+  }
 }
