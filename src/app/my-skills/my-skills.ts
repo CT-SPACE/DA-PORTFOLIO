d5337700463f8e9403c9fcfdb245572bd1be565e
C. Troitzsch <150924499+CT-SPACE@users.noreply.github.com>
2025-12-11 18:21:30 +0100

    MySkills Section generalDesign

--- src/app/my-skills/my-skills.ts
+++ src/app/my-skills/my-skills.ts
@@ -1,11 +1,55 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-my-skills',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './my-skills.html',
   styleUrl: './my-skills.scss',
 })
 export class MySkills {
+public skills: string[] = [
+    'JavaScript',
+    'Typescript',
+    'Angular',
+    'Api',
+    'html',
+    'Css',
 
+    'Bootstrap',
+    'Material-Design',
+
+
+];
+
+public knowledges: string[] = [
+  'Scrum',
+  'CanBan',
+  'Ki-Prompting'
+];
+
+public tools: string[] = [
+  'Visual-Studio-Code',
+  'Figma',
+  'Gimp',
+  'Browser-Dev-Tools',
+      'Git',
+    'Github',
+        'Firebase',
+        'Adservers'
+
+];
+
+
+
+public getKnowledges(): string[]{
+    return this.skills;
+}
+
+public getSkills(): string[]{
+    return this.skills;
+}
+public getTools(): string[]{
+    return this.skills;
+}
 }
