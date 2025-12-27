6c88fa685b57d9cdd58a533c1efe3ad3ad575b27
C. Troitzsch <150924499+CT-SPACE@users.noreply.github.com>
2025-12-27 15:39:54 +0100

    Improving Styles for Privacy / Legal (2) // New Loop Component

---
+++ src/app/shared/loop/loop.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-loop',
+  imports: [],
+  templateUrl: './loop.html',
+  styleUrl: './loop.scss',
+})
+export class Loop {
+
+}
