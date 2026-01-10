f19119087d5650aab9759adff2a108061c41476b
C. Troitzsch <150924499+CT-SPACE@users.noreply.github.com>
2026-01-10 16:04:03 +0100

    TRANSLATE First Version // not working

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,17 +2,26 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Header } from './header/header';
 import { Footer } from "./shared/footer/footer";
+import { TranslateModule } from '@ngx-translate/core';
+
+
+
+
+
 
 
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Header,  Footer],
+  imports: [RouterOutlet, Header,  Footer, TranslateModule],
+  standalone: true,
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
+
 export class App {
 
+ 
 
 }
 
